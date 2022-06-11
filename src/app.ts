6f1a4462f88fd557039c84a5b7afeed0645f4507
Bryan Paik <74121118+bryanpaik@users.noreply.github.com>
2022-06-11 18:02:49 -0400

    dbSetup

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,26 @@
 import express from 'express';
-var app = express();
+
+var expressApp = express();
 
 // Body parser
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+expressApp.use(express.json());
+expressApp.use(express.urlencoded({extended: false}));
 
 // Get from env variable if not just 5000
 const port = process.env.PORT || 5000
 
 // Status
-app.get('/api/status', (req, res) => {
+expressApp.get('/api/status', (req, res) => {
   res.json({status: "up"});
 })
 
 // Use publications router 
-app.use('/api/anime', require('./routes/api/animeApi.ts'));
+expressApp.use('/api/anime', require('./routes/api/animeApi.ts'));
+
+
 
 // Runs the server on the port
-app.listen(port, () => {
+expressApp.listen(port, () => {
   console.log(`Anime web-app listening on port ${port}`)
 })
 
