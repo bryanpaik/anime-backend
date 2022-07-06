9923cdfe3cead741958ee0ae1aa2940f838fc906
Bryan Paik <74121118+bryanpaik@users.noreply.github.com>
2022-07-05 23:38:16 -0400

    Remove firebase api

--- src/app.ts
+++ src/app.ts
@@ -3,26 +3,20 @@ var expressApp = express();
 
 // Body parser
 expressApp.use(express.json());
-expressApp.use(express.urlencoded({extended: false}));
+expressApp.use(express.urlencoded({ extended: false }));
 
 // Get from env variable if not just 5000
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 // Status
 expressApp.get('/api/status', (req, res) => {
-  res.json({status: "up"});
-})
+  res.json({ status: 'up' });
+});
 
-// Use animix router 
+// Use animix router
 expressApp.use('/api/animix', require('./routes/api/animixApi.ts'));
-
-// Use publications router 
-expressApp.use('/api/db', require('./routes/api/firebaseApi.ts'));
 
 // Runs the server on the port
 expressApp.listen(port, () => {
-  console.log(`Anime web-app listening on port ${port}`)
-})
-
-
-
+  console.log(`Anime web-app listening on port ${port}`);
+});
